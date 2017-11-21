07736ae961ddfe1395d9ecb7dbd1d0cc2392d5d2
Avi Arenfeld <aviarenfeld@tests-MacBook-Pro.local>
2017-11-20 21:27:51 -0500

    Initial Commit.

---
+++ src/scripts/index.js
@@ -0,0 +1,32 @@
+import 'jquery.scrollto';
+import { test } from './test';
+
+// $ = jquery "for free".
+$( document ).ready(function(){
+  console.log( 'Ready?' );
+  test();
+
+  $('a.brand').click( function() {
+    e.preventDefault();
+    $(window).scrollTo(3000, { duration: 333 });
+  });
+});
+
+function toggleNavBar() {
+  $('.banner .menu-nav').slideToggle(500);
+}
+$('#menu-toggle').click( toggleNavBar );
+
+$(document).ready(function(){
+  $('#nav-icon3').click(function(){
+    $(this).toggleClass('open');
+  });
+});
+
+window.sr = ScrollReveal();
+//sr.reveal('.splash-item');
+sr.reveal('.resume-wrapper', { duration: 2000 }, 50);
+sr.reveal('.splash-item', { duration: 2000 }, 50);
+sr.reveal('.title', { duration: 1500 }, 50);
+sr.reveal('.portrait', { duration: 1000 }, 50);
+
